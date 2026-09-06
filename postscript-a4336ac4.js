720099672c155e0bab1d1dc3924cd935fb9b3a1a
Hurleveur <belliorum@gmail.com>
2026-09-06 12:23:27 +0200

    deploy 2026-09-06T10:23:27Z

---
+++ postscript-a4336ac4.js
@@ -0,0 +1,17 @@
+await Promise.all([
+  import("./static/scripts/script-0-f7c52b6a.js"),
+  import("./static/scripts/script-1-0e41d61b.js"),
+  import("./static/scripts/script-2-ec880d15.js"),
+  import("./static/scripts/script-3-2b750f2d.js"),
+  import("./static/scripts/script-4-07d05c1c.js"),
+  import("./static/scripts/script-5-2a102aaa.js"),
+  import("./static/scripts/script-6-cc8afa9d.js"),
+  import("./static/scripts/script-7-7e107d7e.js"),
+  import("./static/scripts/script-8-5ef560a8.js"),
+  import("./static/scripts/script-9-c9e900a7.js"),
+  import("./static/scripts/script-10-296b30c3.js"),
+  import("./static/scripts/script-11-be15df42.js"),
+  import("./static/scripts/script-12-88d2c95e.js"),
+  import("./static/scripts/script-13-a2f22c29.js")
+]);
+await import("./static/scripts/script-14-4a1444dc.js");
